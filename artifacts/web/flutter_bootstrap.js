1fae58d1dc97549b820c3339d34912e9e2f3434a
highway-yPark <highway-yPark@users.noreply.github.com>
2024-06-28 09:58:21 +0000

    Commit the artifacts.

--- artifacts/web/flutter_bootstrap.js
+++ artifacts/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "847601074"
+    serviceWorkerVersion: "4145694138"
   }
 });
