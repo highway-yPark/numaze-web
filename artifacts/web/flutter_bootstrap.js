86b99f8d7ae7a88fa440cfe886fe7b247b4d6943
highway-yPark <highway-yPark@users.noreply.github.com>
2024-07-30 11:17:42 +0000

    Commit the artifacts.

--- artifacts/web/flutter_bootstrap.js
+++ artifacts/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1544220480"
+    serviceWorkerVersion: "1540173905"
   }
 });
--- artifacts/web/swipe_back.js
+++ artifacts/web/swipe_back.js
@@ -1,29 +1,29 @@
-function handleTouchMove(event, xStart, yStart) {
-  var xDiff = xStart - event.touches[0].pageX;
-  var yDiff = yStart - event.touches[0].pageY;
-
-  // Detect right swipe
-  if (xDiff < -20 && Math.abs(xDiff) > Math.abs(yDiff)) {
-    event.preventDefault();
-    history.back(); // Navigate to the previous page
-  }
-}
-
-var newHandleTouchMove = function (xStart, yStart) {
-  return function (event) {
-    handleTouchMove(event, xStart, yStart);
-  };
-}
-
-document.addEventListener('touchstart', function (startEvent) {
-  // Ignore multi-touch gestures
-  if (startEvent.touches.length > 1) {
-    return;
-  }
-
-  var xStart = startEvent.touches[0].pageX;
-  var yStart = startEvent.touches[0].pageY;
-
-  // Attach the touchmove event listener
-  document.addEventListener('touchmove', newHandleTouchMove(xStart, yStart), { passive: false });
-}, { passive: false });
+//function handleTouchMove(event, xStart, yStart) {
+//  var xDiff = xStart - event.touches[0].pageX;
+//  var yDiff = yStart - event.touches[0].pageY;
+//
+//  // Detect right swipe
+//  if (xDiff < -20 && Math.abs(xDiff) > Math.abs(yDiff)) {
+//    event.preventDefault();
+//    history.back(); // Navigate to the previous page
+//  }
+//}
+//
+//var newHandleTouchMove = function (xStart, yStart) {
+//  return function (event) {
+//    handleTouchMove(event, xStart, yStart);
+//  };
+//}
+//
+//document.addEventListener('touchstart', function (startEvent) {
+//  // Ignore multi-touch gestures
+//  if (startEvent.touches.length > 1) {
+//    return;
+//  }
+//
+//  var xStart = startEvent.touches[0].pageX;
+//  var yStart = startEvent.touches[0].pageY;
+//
+//  // Attach the touchmove event listener
+//  document.addEventListener('touchmove', newHandleTouchMove(xStart, yStart), { passive: false });
+//}, { passive: false });
