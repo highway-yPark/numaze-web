3288108a3918680508ce8ea2adffd6f3b985763f
yeonpark <144307466+highway-yPark@users.noreply.github.com>
2024-08-10 17:22:59 +0900

    change flutter web plugin

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:numaze_web/common/const/colors.dart';
-import 'package:url_strategy/url_strategy.dart';
+// import 'package:url_strategy/url_strategy.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'common/router.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -15,7 +16,8 @@ import 'firebase_options.dart';
 late FirebaseAnalyticsWeb analytics;
 
 void main() async {
-  setPathUrlStrategy();
+  // setPathUrlStrategy();
+  usePathUrlStrategy();
   // Ensure all widgets are initialized
   WidgetsFlutterBinding.ensureInitialized();
 
