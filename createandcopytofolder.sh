9606c7958e8120145fe9425f0862c6875e83a4ab
yeonpark <144307466+highway-yPark@users.noreply.github.com>
2024-08-10 17:04:36 +0900

    test web script

--- createandcopytofolder.sh
+++ createandcopytofolder.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-[[ -d artifacts ]] && rm -r artifacts
-mkdir artifacts
-cp -R build/web artifacts
+# Remove the existing artifacts directory if it exists
+[[ -d artifacts/web ]] && rm -r artifacts/web
+
+# Create the necessary directories
+mkdir -p artifacts/web
+
+# Copy the contents of the build/web directory to artifacts/web
+cp -R build/web/* artifacts/web/
